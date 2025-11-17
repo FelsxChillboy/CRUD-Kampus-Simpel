340ebcfd6349375bef3d03302442677669154cd1
Whas.felx <azarcoy123@gmail.com>
2025-11-17 13:19:47 +0700

    Delete test_db.py

--- test_db.py
+++
@@ -1,8 +0,0 @@
-from mahasiswa.repo import get_db_connection
-
-conn = get_db_connection()
-if conn:
-    print("✅ Koneksi ke database kampus_db BERHASIL!")
-    conn.close()
-else:
-    print("❌ Gagal konek ke database.")
